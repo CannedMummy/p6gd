b866a4291686d19734efa11571e7c0465193d199
CannedMummy <CannedMummy@gmail.com>
2018-02-12 22:28:01 +0700

    Update

--- resources/views/a-c1.blade.php
+++ resources/views/a-c1.blade.php
@@ -13,13 +13,13 @@
 
       <md-content flex layout-padding>
          <div>
-            <a class="md-primary sub-h1">
+            <div class="md-primary sub-h1">
                1) Danh từ đếm được:
-            </a>
+            </div>
 
-            <a class="md-primary sub-h2">
+            <div class="md-primary sub-h2">
                1.1 Hình thức số ít: a/an + N
-            </a>
+            </div>
 
             - "@highlight(a)" đứng trước N số ít bắt đầu bằng một
             <a class="md-primary text bold">phụ âm</a>.<br>
@@ -30,21 +30,21 @@
 
             Ex: an apple, an orange, an egg, an island, ...<br>
 
-            <a class="md-primary sub-h2">
+            <div class="md-primary sub-h2">
                1.2 Hình thức số nhiều:
-            </a>
+            </div>
 
-            <a class="sub-h3">
+            <div class="sub-h3">
                1.2.1 Hợp quy tắc: N s/es
-            </a>
+            </div>
 
             Ex: books, pens, boxes<br>
 
-            <a class="sub-h3">
+            <div class="sub-h3">
                1.2.2 Bất quy tắc: Số ít
                <i class="material-icons">arrow_forward</i>
                Số nhiều
-            </a>
+            </div>
 
             *a man <i class="material-icons">arrow_forward</i> men<br>
             *a woman <i class="material-icons">arrow_forward</i> women<br>
@@ -61,39 +61,48 @@
             *a criterion <i class="material-icons">arrow_forward</i> criteria<br>
             *a louse <i class="material-icons">arrow_forward</i> lice<br>
 
-            <a class="sub-h3">
+            <div class="sub-h3">
                1.2.3 Danh từ luôn có hình thức và mang nghĩa số nhiều:
                @highlight(clothes, scissors, spectacles, shorts, pants, jeans, trousers, pyjamas, binoculars,
                belongings,) ...
-            </a>
+            </div>
 
-            <a class="sub-h3">
+            <div class="sub-h3">
                1.2.4 Danh từ số ít và số nhiều có hình thức giống nhau:
                @highlight(sheep, deer, aircraft, craft, fish,) ...
-            </a>
+            </div>
 
-            <a class="sub-h3">
+            <div class="sub-h3">
                1.2.5 Danh từ luôn mang nghĩa số nhiều:
                @highlight(police, cattle, people,) ...(xem Bài3, II, 4)
-            </a>
+            </div>
 
-            <a class="sub-h3">
+            <div class="sub-h3">
                1.2.6 Danh từ hình thức số nhưng nhưng mang nghĩa số ít
                @highlight(news, mathematics, United States,) ...(xem,Bài3, I, 2)
-            </a>
+            </div>
 
+            <div class="sub-h1">
+               2) Danh từ không đếm được: là những thứ ta không thể đếm được trưc tiếp (một, 2, ba, ...): như @highlight(milk, water, rice, sugar, butter, salt, bread, news, information, iron, advice, luggage, health,knowledge, equipment, paper, glass,) ...
+            </div>
 
-            2) Danh từ không đế^đứ^^ànr^^igỆhứ ta khôậg thê đếm đươc trưc tiếp (mồt, 2, bắ; ...):hlìư milk, water, rice, sugar, butter, salt, bread, news, information, iron, advice, luggage, health,knowledge, equipment, paper, glass, ...
-            * Chú ý:- Không có hình thức số nhiều (s/es)
-            - Không dùng với a/an/one/two... í
-            - Có thể đi với từ chỉ dơn vị định lượng + of:
+            * Chú ý:- Không có hình thức số nhiều (s/es)<br>
+            - Không dùng với a/an/one/two...<br>
+            - Có thể đi với từ chỉ dơn vị định lượng + of:<br>
             a bottle of milk, two litters of water, a loaf of bread, three sheets of paper,four pieces of chalk, a bar of chocolate, a gallon of petrol, ...
-            3) Danh từ vừa đếm được vừa không đếm được:
+
+            <div class="sub-h1">
+               3) Danh từ vừa đếm được vừa không đếm được:
+            </div>
+
             Một số danh từ trong trường hợp này là đếm được nhưng trong trường hợp khác lại không đếm được.
-            Đếm được —ỳ Không đếm được “a glass (một cái li) —» glass (thủy tinh)
-            “two papers (2 tờ báo) —> paper (giây)
-            *three coffees (ba tách cà phê) —> coffee (cà phê)
-            “two sugars (2 muỗng dương) —ỳ sugar (đường)
+            Đếm được
+            <i class="material-icons">arrow_forward</i>
+            Không đếm được<br>
+            *a glass (một cái li) <i class="material-icons">arrow_forward</i> glass (thủy tinh)
+            *two papers (2 tờ báo) <i class="material-icons">arrow_forward</i> paper (giấy)
+            *three coffees (ba tách cà phê) <i class="material-icons">arrow_forward</i> coffee (cà phê)
+            *two sugars (2 muỗng đường) <i class="material-icons">arrow_forward</i> sugar (đường)
          </div>
       </md-content>
    </div>
